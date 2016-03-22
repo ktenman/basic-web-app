b3fa35547abdb5aa5e947eda3aa490a7c772f143
ktenman <ktenman@gmail.com>
2016-03-22 02:00:17 +0200

    TDD

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,21 +1,26 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 public class CalculatorTest {
 
+    @Mock
     private Calculator calculator;
 
     @Before
     public void setup(){
-        calculator = new Calculator();
+        MockitoAnnotations.initMocks(this);
     }
 
     @Test
     public void testAbs(){
-        assertEquals(4, calculator.abs(-4));
+        when(calculator.abs(-20)).thenReturn(20);
+        assertEquals(20, calculator.abs(-20));
     }
 
 }
