794369172f823151f90d10a3f714fecd964229f6
ktenman <ktenman@gmail.com>
2016-03-22 11:08:28 +0200

    Response JSON

--- src/main/java/tutorial/mvc/BlogEntryController.java
+++ src/main/java/tutorial/mvc/BlogEntryController.java
@@ -4,14 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import tutorial.entities.BlogEntry;
 
 @Controller
 public class BlogEntryController {
+//    @RequestMapping("/test")
+//    public ResponseEntity<Object> test() {
+//        BlogEntry blogEntry = new BlogEntry();
+//        blogEntry.setTitle("Test Blog Entry");
+//        return new ResponseEntity<Object>(blogEntry, HttpStatus.OK);
+//    }
     @RequestMapping("/test")
-    public ResponseEntity<Object> test() {
+    public @ResponseBody BlogEntry test() {
         BlogEntry blogEntry = new BlogEntry();
         blogEntry.setTitle("Test Blog Entry");
-        return new ResponseEntity<Object>(blogEntry, HttpStatus.OK);
+        return blogEntry;
     }
 }
