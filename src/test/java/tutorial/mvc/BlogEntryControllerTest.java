37c4d0a9a42dfffe57ede0510b6fc57e51e20ad5
ktenman <ktenman@gmail.com>
2016-03-22 12:28:15 +0200

    POST MockTest

--- src/test/java/tutorial/mvc/BlogEntryControllerTest.java
+++ src/test/java/tutorial/mvc/BlogEntryControllerTest.java
@@ -5,14 +5,13 @@ import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by Konstantin on 22.03.2016.
@@ -36,7 +35,8 @@ public class BlogEntryControllerTest {
         mockMvc.perform(post("/test")
                 .content("{\"title\":\"Test Blog Title 2\"}")
                 .contentType(MediaType.APPLICATION_JSON)
-        ).andDo(print());
+        ).andExpect(jsonPath("$.title", is("Test Blog Title 2")))
+                .andDo(print());
     }
 
 }
