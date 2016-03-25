559e705b45e30f773a0b56df0178a34560321592
ktenman <ktenman@gmail.com>
2016-03-25 10:42:00 +0200

    Fixed imports

--- src/main/java/tutorial/rest/mvc/BlogEntryController.java
+++ src/main/java/tutorial/rest/mvc/BlogEntryController.java
@@ -1,6 +1,5 @@
 package tutorial.rest.mvc;
 
-import com.sun.org.glassfish.gmbal.ParameterNames;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
